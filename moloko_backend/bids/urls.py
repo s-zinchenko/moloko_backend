797eadb6df4d8a19f9c4c1f838050729da4c7618
saimon zinchenko <zinchieko02@mail.ru>
2023-01-25 12:27:31 +0300

    add new endpoint

--- moloko_backend/bids/urls.py
+++ moloko_backend/bids/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from moloko_backend.bids.views import (
     CooperationBidCreateView,
     LogisticBidCreateView,
+    PriceListBidCreateView,
 )
 
 urlpatterns = [
     path("logistic.create", LogisticBidCreateView.as_view()),
     path("cooperation.create", CooperationBidCreateView.as_view()),
+    path("price_list.create", PriceListBidCreateView.as_view()),
 ]
